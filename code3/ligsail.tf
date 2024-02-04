4f4e6747fb497d8738b5bfc6d47441d44c156bec
bona20 <ahikpob@outlook.com>
2024-02-04 07:34:46 -0500

    region change

--- code3/ligsail.tf
+++ code3/ligsail.tf
@@ -1,6 +1,6 @@
 resource "aws_lightsail_instance" "custom" {
   name              = "week-5"
-  availability_zone = "us-east-1b"
+  availability_zone = "us-east-2"
   blueprint_id      = "amazon_linux_2"
   bundle_id         = "nano_1_0"
   user_data         = "sudo yum install -y httpd && sudo systemctl start httpd && sudo systemctl enable httpd && echo '<h1>Deployed via Terraform dieu est grand et son son fils jesus christ qu<il soit loyer</h1>' | sudo tee /var/www/html/index.html"
